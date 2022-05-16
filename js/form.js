6a524ba3db70631ecb4498477dac3a6e1ef278fd
Brayan <becg852456@gmail.com>
2022-05-15 22:15:46 -0500

    separacion de funciones dentro del boton de ingresar un paciente

--- js/form.js
+++ js/form.js
@@ -1,36 +1,47 @@
 var botonAdicionar = document.querySelector("#adicionar-paciente");
 
-botonAdicionar.addEventListener("click",function(event){
+botonAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
 
     var form = document.querySelector("#form-adicionar");
-    
-    var nombre = form.nombre.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value
-    
+
+    var paciente = capturarDatosPaciente(form);
+
+
     var tabla = document.querySelector("#tabla-pacientes");
 
-    pacienteTr = document.createElement("tr");     
+    //crear los tds de un tr
+    pacienteTr = document.createElement("tr");
     nombreTd = document.createElement("td");
     alturaTd = document.createElement("td");
     pesoTd = document.createElement("td");
     gorduraTd = document.createElement("td");
     imcTd = document.createElement("td");
 
+    //Asignar valores a la propiedad textContent
     nombreTd.textContent = nombre;
     alturaTd.textContent = altura;
     pesoTd.textContent = peso;
     gorduraTd.textContent = gordura;
-    imcTd.textContent = calcularIMC(peso,altura);
+    imcTd.textContent = calcularIMC(peso, altura);
 
+    //Asignacion de los tds al tr de la fila
     pacienteTr.appendChild(nombreTd);
     pacienteTr.appendChild(pesoTd);
     pacienteTr.appendChild(alturaTd);
     pacienteTr.appendChild(gorduraTd);
     pacienteTr.appendChild(imcTd);
 
+    //Asignacion del objeto Tr(FILA) a la tabla
     tabla.appendChild(pacienteTr);
 
 });
+
+function capturarDatosPaciente(form) {
+    var nombre = form.nombre.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value
+
+    return
+}
