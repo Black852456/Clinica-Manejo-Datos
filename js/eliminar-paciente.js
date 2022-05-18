03c50bf5de34d9e055ec221b72b9257365f4e016
Brayan <becg852456@gmail.com>
2022-05-18 16:03:36 -0500

    Agregando elementos de eliminacion y opacidad para efectos visuales

--- js/eliminar-paciente.js
+++ js/eliminar-paciente.js
@@ -1,11 +1,15 @@
 var pacientes = document.querySelectorAll(".paciente");
 var tabla = document.querySelector("#tabla-pacientes");
 
-tabla.addEventListener("click", function(event) {
+tabla.addEventListener("dblclick", function(event) {
     //console.log(event.target);
     //.target => (SELECCIONA EL ELEMENTO MAS MINIMO AL QUE LE ESTA HACIENDO CLICK)
     //.parentNode => (SELECCIONA EL ELEMENTO PADRE QUE HALLA DETECTADO EL .TARGET)
-    event.target.parentNode.remove();
+
+    event.target.parentNode.classList.add("fadeOut");
+    setTimeout(function() {
+        event.target.parentNode.remove();
+    }, 500);
 });
 
 /*
